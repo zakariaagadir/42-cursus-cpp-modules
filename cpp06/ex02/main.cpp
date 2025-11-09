df842178e1ce66dc0d69c193fba8f85033cfe6db
zakariaagadir <zakariamounji44499@gmail.com>
2025-11-09 15:58:26 +0100

    add more functions

---
+++ cpp06/ex02/main.cpp
@@ -0,0 +1,73 @@
+#include "Base.hpp"
+#include "B.hpp"
+#include "A.hpp"
+#include "C.hpp"
+#include <iostream>
+#include <cstdlib>
+#include <ctime> 
+
+Base* generate(void)
+{
+    int r = rand() % 3;
+
+    switch (r) {
+        case 0:
+            std::cout << "Generated A" << std::endl;
+            return new A;
+        case 1:
+            std::cout << "Generated B" << std::endl;
+            return new B;
+        default:
+            std::cout << "Generated C" << std::endl;
+            return new C;
+    }
+}
+
+void identify(Base* p)
+{
+    if (dynamic_cast<A*>(p))
+        std::cout << "A" << std::endl;
+    else if (dynamic_cast<B*>(p))
+        std::cout << "B" << std::endl;
+    else if (dynamic_cast<C*>(p))
+        std::cout << "C" << std::endl;
+    else
+        std::cout << "Unknown type" << std::endl;
+}
+
+void identify(Base& p)
+{
+    try {
+        (void)dynamic_cast<A&>(p);
+        std::cout << "A" << std::endl;
+        return;
+    } catch (...) {}
+
+    try {
+        (void)dynamic_cast<B&>(p);
+        std::cout << "B" << std::endl;
+        return;
+    } catch (...) {}
+
+    try {
+        (void)dynamic_cast<C&>(p);
+        std::cout << "C" << std::endl;
+        return;
+    } catch (...) {}
+}
+
+int main()
+{
+    srand(time(0));
+    Base* ptr = generate();
+
+    std::cout << "identify(Base*): ";
+    identify(ptr);
+
+    std::cout << "identify(Base&): ";
+    identify(*ptr);
+
+    delete ptr;
+}
+
+
