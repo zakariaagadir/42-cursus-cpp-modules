fc6a3a4e732d7994ca4821f94ef4ee0d5253b8aa
zakariaagadir <zakariamounji44499@gmail.com>
2025-11-23 22:39:03 +0100

    add some

--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -4,7 +4,18 @@
 
 
 
+class Span{
+public:
+    Span(unsigned int n);
+    Span(const Span& other);
+    Span& operator=(const Span& other);
+    ~Span();
 
+    void addNumber(int number);
+    void addNumbers(int* numbers, unsigned int count);
+    unsigned int shortestSpan() const;
+    unsigned int longestSpan() const;
+};
 
 
 
