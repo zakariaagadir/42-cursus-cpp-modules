738ee75baffd565a4ac6574fa9e4921d07059159
zakaria <zakariamounji44499@gmail.com>
2025-11-24 00:27:42 +0100

    add some futures

--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -1,20 +1,25 @@
 #ifndef SPAN_HPP
 #define SPAN_HPP
+#include <vector>
+#include <iostream>
 
 
 
 
 class Span{
-public:
-    Span(unsigned int n);
-    Span(const Span& other);
-    Span& operator=(const Span& other);
-    ~Span();
+    private:
+        unsigned int max_;
+        std::vector<int> data_;
+    public:
+        Span(unsigned int n);
+        Span(const Span& other);
+        Span& operator=(const Span& other);
+        ~Span();
 
-    void addNumber(int number);
-    void addNumbers(int* numbers, unsigned int count);
-    unsigned int shortestSpan() const;
-    unsigned int longestSpan() const;
+        void addNumber(int number);
+        void addNumbers(int* numbers, unsigned int count);
+        unsigned int shortestSpan() const;
+        unsigned int longestSpan() const;
 };
 
 
