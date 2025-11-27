8aa6619290feeb8693582dbe6bb637ed18776df9
zakariaagadir <zakariamounji44499@gmail.com>
2025-11-27 23:22:17 +0100

    add other

---
+++ cpp08/ex02/MutantStack.cpp
@@ -0,0 +1,4 @@
+#include "MutantStack.hpp"
+
+
+
--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -1,10 +1,29 @@
-#ifndef MUTANTSTACK_hpp
-#define MUTANTSTACK_hpp
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
 
-class MutantStack{
-    
-}
+#include <stack>
 
+template <class T>
+class MutantStack : public std::stack<T>
+{
+    public:
+        MutantStack() {}
+        MutantStack(const MutantStack &other) : std::stack<T>(other) {}
+        MutantStack &operator=(const MutantStack &other)
+        {
+            std::stack<T>::operator=(other);
+            return *this;
+        }
+        ~MutantStack() {}
+
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+        iterator begin() { return std::stack<T>::c.begin(); }
+        iterator end()   { return std::stack<T>::c.end(); }
+
+        const_iterator begin() const { return std::stack<T>::c.begin(); }
+        const_iterator end()   const { return std::stack<T>::c.end(); }
+};
 
 #endif
-
