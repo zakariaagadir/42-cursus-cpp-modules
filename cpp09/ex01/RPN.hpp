96f75485edc44c06ed5ff3266ad0ead3d494d3cd
zakaria <zakariamounji44499@gmail.com>
2025-12-04 16:37:30 +0100

    add other exercise

---
+++ cpp09/ex01/RPN.hpp
@@ -0,0 +1,17 @@
+#ifndef RPN_HPP
+#define RPN_HPP
+
+#include"RPN.hpp"
+#include<stack>
+#include<iostream>
+#include<string>
+
+bool isOperator(int c);
+int makeoper(int a,int b, char c);
+
+
+
+
+
+
+#endif
--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -1,4 +1,4 @@
-#include"RPN.hpp"
+#include "RPN.hpp"
 
 
 
@@ -6,6 +6,49 @@
 
 int main(int argc, char* argv[])
 {
-    
+    std::stack<int> stac;
+    if(argc != 2)
+    {
+        std::cerr<<"error 1"<<std::endl;
+        return(1);
+    }
+    for (int i = 1; i < argc; i++)
+    {
+        std::string s = argv[i];
+        for(size_t j = 0; j<s.size(); j++)
+        {
+            if(isdigit(s[j]))
+            {
+                stac.push(s[j] - '0');
+            } else
+            {
+                if(isOperator(s[j]))
+                {
+                    if(stac.size() < 2)
+                    {
+                        std::cerr << "Error 2"<< std::endl;
+                        return(1);
+                    }
+                    int a = stac.top();
+                    stac.pop();
+                    int b = stac.top();
+                    stac.pop();
+                    int res = makeoper(a,b, s[j]);
+                    stac.push(res);
+                } else if(s[j] != ' ')
+                {
+                    std::cerr << "Error 3"<< std::endl;
+                    return(1);
+                }
+            }
+
+        }
+    }
+    if(stac.size() != 1)
+    {
+        std::cerr << "Error "<< std::endl;
+        return(1);
+    }
+    std::cout << stac.top()<< std::endl;
     return(0);
 }
